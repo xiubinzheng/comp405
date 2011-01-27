641f7cb06c58129f86a33e984cb4809e6a09abcc
stodolli <stodolli@gmail.com>
2011-01-27 15:51:47 +0000

    Analyzer stuff

--- TestFirstExercise/src/testfirst/Analyzer.java
+++ TestFirstExercise/src/testfirst/Analyzer.java
@@ -5,25 +5,25 @@ import java.util.ArrayList;
 
 public class Analyzer
 {
-	private ArrayList <Car> m_total;
-	private ArrayList <Car> m_type;
-	private ArrayList <Car> m_color;
-	
-	public Analyzer(ArrayList <Car> carList)
+	private ArrayList<Car>	m_total;
+	private ArrayList<Car>	m_type;
+	private ArrayList<Car>	m_color;
+
+	public Analyzer(ArrayList<Car> carList)
 	{
-		m_total=carList;
+		m_total = carList;
 	}
-	
+
 	public Color lowestAverageColor()
 	{
 		ArrayList<Car> remainingCarList = new ArrayList<Car>();
-		remainingCarList=m_total;
+		remainingCarList = m_total;
 		Color answer = null;
 		double avg;
-		
+
 		return answer;
 	}
-	
+
 	public Car.Type lowestAverageType()
 	{
 		return Car.Type.hatchback;
