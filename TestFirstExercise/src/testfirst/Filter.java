d12cc15ff5c7880ed167704469dcb3a9e356aced
LHUMCohen <LHUMCohen@2bc14a36-1bbf-96bb-0caf-a0df89e36292>
2011-02-24 15:03:22 +0000

    Tag for first release

--- TestFirstExercise/src/testfirst/Filter.java
+++
@@ -1,9 +0,0 @@
-package testfirst;
-
-import java.awt.Color;
-import java.util.LinkedList;
-
-public interface Filter<Type>
-{
-	LinkedList<Car> filter(Type type, LinkedList<Car> carList);
-}
--- TestFirstExercise/src/testfirst/SortDoors.java
+++
@@ -1,6 +0,0 @@
-package testfirst;
-
-public class SortDoors
-{
-
-}
