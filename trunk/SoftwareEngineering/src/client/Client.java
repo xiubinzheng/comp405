711ce2f58f3b491cc25e5715414e0c134ee150b2
stodolli@gmail.com <stodolli@gmail.com@2bc14a36-1bbf-96bb-0caf-a0df89e36292>
2011-03-24 14:05:49 +0000

    Client modified - no code should break or be affected outside of the class !!

--- trunk/SoftwareEngineering/src/client/Client.java
+++ trunk/SoftwareEngineering/src/client/Client.java
@@ -4,30 +4,26 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
-
 import project.Project;
-
-import java.util.Set;
 
 public class Client
 {
-	private int		m_clientID			= 0;
-	private String	m_clientName		= "";
-	private String	m_clientDescription	= "";
-	private HashMap<Integer, Project> m_projects = new HashMap<Integer, Project>();
-	
-	int m_tempProjectKey = -1; //dec with each use
-	
+	private int							m_clientID			= 0;
+	private String						m_clientName		= "";
+	private String						m_clientDescription	= "";
+	private HashMap<Integer, Project>	m_projects			= new HashMap<Integer, Project>();
+
 	/**
 	 * Create a new Client with default attributes.
 	 */
 	public Client()
 	{
-		
+
 	}
-	
+
 	/**
 	 * Create a client with the provided arguments.
+	 * 
 	 * @param clientID ID of the client.
 	 * @param name Name of the client.
 	 * @param description Description of the client.
@@ -41,38 +37,33 @@ public class Client
 
 	/**
 	 * Create a client with the provided arguments.
+	 * 
 	 * @param clientID ID of the client.
 	 * @param name Name of the client.
 	 * @param description Description of the client.
 	 * @param projects Linked List of projects.
 	 * @param projects should already have an ID set
 	 */
-	public Client(int clientID, String name, String description, LinkedList<Project> projects)
+	public Client(int clientID, String name, String description,
+			LinkedList<Project> projects)
 	{
 		m_clientID = clientID;
 		m_clientName = name;
 		m_clientDescription = description;
-		for(Project project : projects)
+		for (Project project : projects)
 			m_projects.put(project.getProjectID(), project);
 	}
-	
+
 	/**
 	 * Add project. Duplicates are ignored.
+	 * 
 	 * @param project
 	 */
 	public boolean addProject(Project project)
 	{
-		if(!m_projects.containsKey(project.getProjectID()))
+		if (!m_projects.containsKey(project.getProjectID()))
 		{
-			if(project.getProjectID() < 0)
-			{
-				m_projects.put(m_tempProjectKey, project);
-				--m_tempProjectKey;
-			}
-			else
-			{
-				m_projects.put(project.getProjectID(), project);
-			}
+			m_projects.put(project.getProjectID(), project);
 			return true;
 		}
 		else
@@ -80,15 +71,16 @@ public class Client
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Remove project.
+	 * 
 	 * @param projID The ID number of the project to be removed.
 	 * @return True if removed project existed and was removed, false if not.
 	 */
 	public boolean removeProject(int projID)
 	{
-		if(m_projects.containsKey(projID))
+		if (m_projects.containsKey(projID))
 		{
 			m_projects.remove(projID);
 			return true;
@@ -98,14 +90,15 @@ public class Client
 			return false;
 		}
 	}
-	
+
 	/**
 	 * populates a passed list with a client's projects.
+	 * 
 	 * @param projectList
 	 */
 	public void getProjectList(ArrayList<Project> projectList)
 	{
-		//I N C E P T I O N level 3 watch out for limbo!
+		// I N C E P T I O N level 3 watch out for limbo!
 		Collection<Project> projects = m_projects.values();
 		projectList.clear();
 		projectList.addAll(projects);
