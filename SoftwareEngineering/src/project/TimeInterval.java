f4e23727bb02370def46e2eefc33249e5977a0a4
stodolli <stodolli@gmail.com>
2011-03-29 20:04:40 +0000

    TimeID and ProjectID fields and respective set and get methods were added.

--- SoftwareEngineering/src/project/TimeInterval.java
+++ SoftwareEngineering/src/project/TimeInterval.java
@@ -1,35 +1,85 @@
+/**
+ * TODO: Javadocs class description
+ */
+
 package project;
 
 import java.util.Date;
 
 public class TimeInterval
 {
-	Date m_start;
-	Date m_stop;
-	
+	private int			m_timeID;
+	private int			m_projectID;
+	Date				m_start;
+	Date				m_stop;
+
+	private static int	m_defaultTimeID	= -1;
+
 	public TimeInterval()
 	{
 		m_start = null;
 		m_stop = null;
+		m_timeID = m_defaultTimeID--;
+
+		// revisit later
+		m_projectID = 0;
 	}
-	
+
+	public TimeInterval(int projectID)
+	{
+		m_start = null;
+		m_stop = null;
+		m_timeID = m_defaultTimeID--;
+		m_projectID = projectID;
+	}
+
 	public void start()
 	{
 		m_start = new Date();
 	}
-	
+
 	public void stop()
 	{
 		m_stop = new Date();
 	}
-	
+
 	public Date getStart()
 	{
 		return m_start;
 	}
-	
+
 	public Date getStop()
 	{
 		return m_stop;
+	}
+
+	public int getTimeID()
+	{
+		return m_timeID;
+	}
+
+	public int getProjectID()
+	{
+		return m_projectID;
+	}
+
+	/**
+	 * 
+	 * @param timeID
+	 */
+	public void setTimeID(int timeID)
+	{
+		assert m_timeID <= 0;
+		m_timeID = timeID;
+	}
+
+	/**
+	 * 
+	 * @param projectID
+	 */
+	public void setProjectID(int projectID)
+	{
+		assert m_projectID <= 0;
+		m_projectID = projectID;
 	}
 }
