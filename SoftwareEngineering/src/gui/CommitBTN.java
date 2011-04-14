1a8ffcd071756083a32667bb0b3bf662ccfcfcc7
LHUMCohen <LHUMCohen@2bc14a36-1bbf-96bb-0caf-a0df89e36292>
2011-04-14 14:35:14 +0000

    Refactor

---
+++ SoftwareEngineering/src/gui/CommitBTN.java
@@ -0,0 +1,23 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class CommitBTN extends JButton implements ActionListener 
+{
+	MainGUI m_gui;
+	public CommitBTN(MainGUI gui)
+	{
+		m_gui = gui;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		// TODO implement code to call a commit of a time interval
+		
+		
+	}
+}
---
+++ SoftwareEngineering/src/gui/HoursSummaryPNL.java
@@ -0,0 +1,10 @@
+package gui;
+
+public class HoursSummaryPNL {
+
+	public HoursSummaryPNL()
+	{
+		
+	}
+	
+}
