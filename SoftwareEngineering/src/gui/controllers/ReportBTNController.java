c71d4fe670a84d07e2d0582b3f7e8d999848dfd9
LHUMCohen <LHUMCohen@2bc14a36-1bbf-96bb-0caf-a0df89e36292>
2011-04-14 14:21:27 +0000

    Refactor

---
+++ SoftwareEngineering/src/gui/controllers/ReportBTNController.java
@@ -0,0 +1,23 @@
+package gui.controllers;
+
+import gui.HTMLViewer;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class ReportBTNController implements ActionListener
+{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		try {
+			HTMLViewer htmlViewer = new HTMLViewer();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
